5a29ce0dc8ccaa1c70d467125123d315567621b0
tianzhiheng <909833599@qq.com>
2023-05-17 16:10:35 +0800

    完善签名

--- src/main/kotlin/com/arnold/signature/EditConfigAction.kt
+++ src/main/kotlin/com/arnold/signature/EditConfigAction.kt
@@ -1,11 +1,11 @@
 package com.arnold.signature
 
-import com.arnold.signature.ui.EditConfigView
+import com.arnold.signature.ui.SignatureConfigView
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class EditConfigAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        EditConfigView().setVisible(true);
+        SignatureConfigView().setVisible(true);
     }
 }
